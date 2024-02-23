189e427db635175ae474c4161588baf44e2c5302
nukhes <nukhes@protonmail.com>
2024-02-23 12:05:20 -0300

    basico

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt_BR">
-      <body className={inter.className}>{children}</body>
+      <body class="bg-slate-900 flex justify-center" className={inter.className}>{children}</body>
     </html>
   );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,23 @@
 export default function Home() {
   return (
-    <div></div>
+    <div className="
+    w-[95vw] h-[95vh] max-w-screen-2xl bg-slate-950 mt-5 text-slate-50
+    grid grid-flow-col grid-cols-3
+    ">
+
+      <aside className="bg-slate-800">
+        <header className="bg-slate-700 h-16">
+          header
+        </header>
+
+        aside
+
+      </aside>
+      
+      <div className="bg-slate-950 col-span-2">
+        content
+      </div>
+
+    </div>
   );
 }
